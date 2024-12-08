d041d6728a58aeafe479bfadd6097e6bb528ea38
Abdulsamed Arslan <samedarslan0723@gmail.com>
2024-12-09 00:57:46 +0300

    Tabbar created

---
+++ ExpenseTracker/Presentation/CustomTabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBarView.swift
+//  ExpenseTracker
+//
+//  Created by Abdulsamed Arslan on 9.12.2024.
+//
+
+import SwiftUI
+
+struct CustomTabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBarView()
+}
