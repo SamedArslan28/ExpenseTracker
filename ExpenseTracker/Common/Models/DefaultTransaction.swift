45fbabc1f2c8258f5920eef8cbd3864059896296
Abdulsamed Arslan <samedarslan0723@gmail.com>
2025-04-09 20:02:07 +0300

    - TransactionItem renamed to DefaultTransaction. - Codable procotol and implementations removed from both TransactionModel.

---
+++ ExpenseTracker/Common/Models/DefaultTransaction.swift
@@ -0,0 +1,61 @@
+//
+//  BalanceItem.swift
+//  ExpenseTracker
+//
+//  Created by Abdulsamed Arslan on 9.12.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class DefaultTransaction: BaseTransaction {
+    @Attribute(.unique) var id: UUID
+    var name: String
+    var category: TransactionCategory
+    var amount: Double
+    var isExpense: Bool
+    var date: Date
+
+    static var getAll: FetchDescriptor<DefaultTransaction> {
+        FetchDescriptor<DefaultTransaction>()
+    }
+
+    init(
+        id: UUID = UUID(),
+        name: String,
+        category: TransactionCategory,
+        amount: Double,
+        isExpense: Bool,
+        isFixed: Bool = false,
+        date: Date,
+        day: Int? = nil
+    ) {
+        self.id = id
+        self.name = name
+        self.category = category
+        self.amount = amount
+        self.isExpense = isExpense
+        self.date = date
+    }
+}
+
+// TODO: - Move this logic to related part
+
+extension DefaultTransaction {
+    static func convertToCSV(transactions: [DefaultTransaction]) -> String {
+        var csvString = "id,name,category,amount,isExpense,isFixed,date,day\n"
+        for transaction in transactions {
+            let id = transaction.id.uuidString
+            let name = transaction.name
+            let category = transaction.category.rawValue
+            let amount = transaction.amount
+            let isExpense = transaction.isExpense ? "true" : "false"
+            let date = ISO8601DateFormatter().string(from: transaction.date)
+            let row = "\(id),\(name),\(category),\(amount),\(isExpense),\(date)\n"
+            csvString.append(row)
+        }
+        return csvString
+    }
+}
+
--- ExpenseTracker/Common/Models/FixedTransaction.swift
+++ ExpenseTracker/Common/Models/FixedTransaction.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-class FixedTransaction: BaseTransaction, Codable {
+class FixedTransaction: BaseTransaction {
 
     @Attribute(.unique)
     var id: UUID
@@ -20,16 +20,6 @@ class FixedTransaction: BaseTransaction, Codable {
     var isExpense: Bool
     var category: TransactionCategory
 
-    enum CodingKeys: CodingKey {
-        case _id
-        case _name
-        case _date
-        case _amount
-        case _day
-        case _isExpense
-        case _category
-    }
-
     init(id: UUID, name: String, date: Date, amount: Double, day: Int, isExpense: Bool, category: TransactionCategory) {
         self.id = id
         self.name = name
@@ -38,28 +28,6 @@ class FixedTransaction: BaseTransaction, Codable {
         self.day = day
         self.isExpense = isExpense
         self.category = category
-    }
-
-    required init (from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(UUID.self, forKey: ._id)
-        name = try container.decode(String.self, forKey: ._name)
-        date = try container.decode(Date.self, forKey: ._date)
-        amount = try container.decode(Double.self, forKey: ._amount)
-        day = try container.decode(Int.self, forKey: ._day)
-        isExpense = try container.decode(Bool.self, forKey: ._isExpense)
-        category = try container.decode(TransactionCategory.self, forKey: ._category)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: ._id)
-        try container.encode(name, forKey: ._name)
-        try container.encode(date, forKey: ._date)
-        try container.encode(amount, forKey: ._amount)
-        try container.encode(day, forKey: ._day)
-        try container.encode(isExpense, forKey: ._isExpense)
-        try container.encode(category, forKey: ._category)
     }
 
     static var getAll: FetchDescriptor<FixedTransaction> {
