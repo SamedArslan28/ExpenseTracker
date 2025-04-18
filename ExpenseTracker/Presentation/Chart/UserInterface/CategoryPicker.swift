c31c969ca910bd403324ad75362690c710f9b4f5
Abdulsamed Arslan <samedarslan0723@gmail.com>
2025-04-18 20:13:03 +0300

    - Expense Chart view seperated. - Slow build problem solved. - Category picker seperated.

---
+++ ExpenseTracker/Presentation/Chart/UserInterface/CategoryPicker.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryPicker.swift
+//  ExpenseTracker
+//
+//  Created by Abdulsamed Arslan on 4/18/25.
+//
+
+import SwiftUI
+
+struct CategoryPicker: View {
+    @Binding var selectedCategory: TransactionCategory
+
+    private let categories = TransactionCategory.allCases.filter { $0 != .fixedExpenses }
+
+    var body: some View {
+        HStack {
+            Text("Select a Category")
+                .font(.headline)
+            Spacer()
+            Picker("Category", selection: $selectedCategory.animation(.easeInOut)) {
+                ForEach(categories, id: \.self) { category in
+                    HStack {
+                        Image(systemName: category.iconName)
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(category.color)
+                        Text(category.rawValue.capitalized)
+                    }
+                }
+            }
+            .pickerStyle(.menu)
+        }
+    }
+}
--- ExpenseTracker/Presentation/Chart/UserInterface/TransactionChartSectionView.swift
+++ ExpenseTracker/Presentation/Chart/UserInterface/TransactionChartSectionView.swift
@@ -8,57 +8,13 @@
 import SwiftUI
 import Charts
 
-enum ChartType: String, CaseIterable, Identifiable {
-    case pie = "Pie Chart"
-    case bar = "Bar Chart"
-
-    var id: Self { self }
-}
-
 struct TransactionChartSectionView: View {
     let title: String
     let items: [any BaseTransaction]
 
-    @State private var selectedChart: ChartType = .pie
-
-    private var groupedData: [(key: TransactionCategory, value: Double)] {
-        Dictionary(grouping: items.filter { $0.isExpense }, by: { $0.category })
-            .map { (key: $0.key, value: $0.value.reduce(0) { $0 + $1.amount }) }
-    }
-
-    private var totalExpense: Double {
-        items.filter { $0.isExpense }
-            .reduce(0) { $0 + $1.amount }
-    }
-
     var body: some View {
         VStack(spacing: 16) {
-
             MonthlyExpense()
-//            Text(title)
-//                .font(.title2.bold())
-//                .frame(maxWidth: .infinity, alignment: .leading)
-//                .padding(.horizontal)
-//
-//            Picker("Chart Type", selection: $selectedChart) {
-//                ForEach(ChartType.allCases) { chart in
-//                    Text(chart.rawValue).tag(chart)
-//                }
-//            }
-//            .pickerStyle(.segmented)
-//            .padding(.horizontal)
-//
-//            switch selectedChart {
-//                case .pie:
-//                    PieChart(
-//                        groupedData: groupedData,
-//                        totalExpense: totalExpense
-//                    )
-//                case .bar:
-//                    BarChart(
-//                        groupedData: groupedData
-//                    )
-//            }
         }
         .padding()
         .background(
